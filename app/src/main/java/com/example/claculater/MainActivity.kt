6f24b9ee6b5b2871e8380f46b809ac2a2d753c45
Mohammed Ali <mohammed.mizban97@gmail.com>
2024-05-07 11:03:30 +0300

    delete previous dead code

--- app/src/main/java/com/example/claculater/MainActivity.kt
+++ app/src/main/java/com/example/claculater/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.claculater
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -9,7 +8,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import javax.security.auth.login.LoginException
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
