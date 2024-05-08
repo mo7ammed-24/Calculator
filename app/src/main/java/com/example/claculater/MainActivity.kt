1ed002230402c39c753080efc837afe3ef66843b
Mohammed Ali <mohammed.mizban97@gmail.com>
2024-05-08 10:03:45 +0300

    feature implemented

--- app/src/main/java/com/example/claculater/MainActivity.kt
+++ app/src/main/java/com/example/claculater/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.claculater
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +26,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var textNumber : TextView
     lateinit var buttonClear : Button
     var lastNumber: Double = 0.0
+    var result :Double = 0.0
     var currentOperation:Operation? = null
+    lateinit var textOperation :TextView
+    val operationMap = mapOf(Operation.PLUS to "+",Operation.MINUS to "-", Operation.MULTIPLICATION to "×", Operation.DIVISION to "÷", Operation.MODULES to "%")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -31,10 +40,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         initView()
         addCallBacks()
     }
+
 
     fun addCallBacks(){
         buttonPlus.setOnClickListener {
@@ -53,23 +62,24 @@ class MainActivity : AppCompatActivity() {
             prepareOperation(Operation.MODULES)
         }
         buttonResult.setOnClickListener {
+            currentOperation?.let {
+                if (lastNumber !=0.0 && textNumber.text.toString().toDouble() != 0.0){
+                    result = doCurrentOperation()
+                    textNumber.text = result.toString()
+                }
 
-            if (lastNumber !=0.0 && textNumber.text.toString().toDouble() != 0.0){
-                val result = doCurrentOperation()
-                textNumber.text = result.toString()
+                lastNumber = 0.0
             }
-
-            lastNumber = 0.0
         }
 
 
         buttonClear.setOnClickListener {
             clearInput()
+            textOperation.text = ""
         }
     }
 
     private fun doCurrentOperation():Double{
-
         val secondNumber = textNumber.text.toString().toDouble()
         return  when(currentOperation){
             Operation.PLUS -> lastNumber + secondNumber
@@ -86,11 +96,12 @@ class MainActivity : AppCompatActivity() {
         buttonMinus = findViewById(R.id.minusButton)
         buttonMultiplication = findViewById(R.id.muliplyButton)
         buttonDiv = findViewById(R.id.divButton)
+        textNumber = findViewById(R.id.resultTextView)
         buttonModules = findViewById(R.id.modulesButton)
         buttonResult = findViewById(R.id.resultButton)
-        textNumber = findViewById(R.id.resultTextView)
         buttonClear = findViewById(R.id.deleteButton)
         buttonNegative = findViewById(R.id.negativeButton)
+        textOperation = findViewById(R.id.operationTextView)
     }
 
 
@@ -99,14 +110,19 @@ class MainActivity : AppCompatActivity() {
         val oldNumber = textNumber.text.toString()
         val newTextNumber = oldNumber + newDigit
         textNumber.text =newTextNumber
+        prepareOperationText(newDigit)
     }
     fun clearInput(){
         textNumber.text =""
     }
-
     fun prepareOperation(operation: Operation){
         if(textNumber.text != ""){
-            lastNumber = textNumber.text.toString().toDouble()
+
+            val temp1  = textNumber.text.toString().toDoubleOrNull()
+            lastNumber = temp1!!
+            val temp2 = lastNumber.toString() + operationMap.get(operation).toString()
+            textOperation.text = temp2
+
         }
         clearInput()
         currentOperation = operation
@@ -119,5 +135,43 @@ class MainActivity : AppCompatActivity() {
         val newTextNumber = negSign + oldNumber
         textNumber.text =newTextNumber
     }
+
+    fun prepareOperationText(newTextNumber:String){
+        if(currentOperation!=null){
+            textOperation.text = textOperation.text.toString() + newTextNumber
+            result = doCurrentOperation()
+            textNumber.text = result.toString()
+            currentOperation = null
+        }
+        else {
+            val temp = textOperation.text.toString()
+            val temp2 = temp + newTextNumber
+            textOperation.text = temp2
+        }
+    }
+
+    fun onClickDot(v: View) {
+        val oldNumber = textNumber.text.toString()
+        if (textNumber.text.toString().find { it == '.' } == '.')
+            textNumber.text = oldNumber
+        else if (textNumber.text.toString() == "") {
+            prepareOperationText("0.")
+            textNumber.text = "0."
+
+        } else {
+            val negDot = "."
+            val newTextNumber = oldNumber + negDot
+            prepareOperationText(negDot)
+            textNumber.text = newTextNumber
+        }
+    }
+
+    fun checkPassword(){
+        if(textNumber.text.toString() == ""){
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+
 
 }
